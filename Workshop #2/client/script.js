6914471aed2e4cc046ee3b3812ffb4f9f54ed848
Luis Andrés Gómez Salas <113403512+Makshen555@users.noreply.github.com>
2023-06-14 23:47:30 -0600

    Add files via upload

--- Workshop #2/client/script.js
+++ Workshop #2/client/script.js
@@ -5,7 +5,8 @@ formElement.addEventListener("submit", (event) => {
     let firstName = document.getElementById("first_name").value;
     let lastName = document.getElementById("last_name").value;
     let cedula = document.getElementById("cedula").value;
-    let age = document.getElementById("age").value;
+    let años = document.getElementById("age").value;
+    let age = parseInt(años);
     let teacher = {
         first_name : firstName,
         last_name : lastName,
@@ -14,9 +15,10 @@ formElement.addEventListener("submit", (event) => {
     }
     let teacherJSON = JSON.stringify(teacher);
     console.log(teacherJSON);
-    fetch('http://localhost:3001/api/teachers', {
-        method : 'Post',
+    fetch("http://localhost:3001/api/teachers", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",},
         body : teacherJSON
-    })
-}
-)
+    });
+});
